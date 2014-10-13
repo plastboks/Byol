7369665b3660d340694fcb4df30fba50363fa305
Alexander Skjolden <alex@plastboks.net>
2014-10-13 17:23:38 +0200

    added more arithmetic and min max functions
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- src/parsing.c
+++ src/parsing.c
@@ -30,6 +30,22 @@ long eval(mpc_ast_t* t)
     return x;
 }
 
+int min(int x, int y)
+{
+    if (x < y)
+        return x;
+    else
+        return y;
+}
+
+int max(int x, int y)
+{
+    if (x > y)
+        return x;
+    else
+        return y;
+}
+
 long eval_op(long x, char* op, long y)
 {
     if (strcmp(op, "+") == 0) { return x + y; }
@@ -38,7 +54,8 @@ long eval_op(long x, char* op, long y)
     if (strcmp(op, "/") == 0) { return x / y; }
     if (strcmp(op, "%") == 0) { return x % y; }
     if (strcmp(op, "^") == 0) { return pow(x, y); }
-
+    if (strcmp(op, "min") == 0) { return min(x, y); }
+    if (strcmp(op, "max") == 0) { return max(x, y); }
     return 0;
 }
 
@@ -52,11 +69,11 @@ int main(int argc, char** argv)
 
     /* Define them with the following Language */
     mpca_lang(MPCA_LANG_DEFAULT,
-      "                                                     \
-        number   : /-?[0-9]+/ ;                             \
-        operator : '+' | '-' | '*' | '/' | '%' | '^' ;      \
-        expr     : <number> | '(' <operator> <expr>+ ')' ;  \
-        lispy    : /^/ <operator> <expr>+ /$/ ;             \
+      "                                                                     \
+        number   : /-?[0-9]+/ ;                                             \
+        operator : '+' | '-' | '*' | '/' | '%' | '^' | \"min\" | \"max\";   \
+        expr     : <number> | '(' <operator> <expr>+ ')' ;                  \
+        lispy    : /^/ <operator> <expr>+ /$/ ;                             \
       ",
       Number, Operator, Expr, Lispy);
 
