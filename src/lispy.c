f47a8888867427f013d16ccb95d0f52cdba43f3f
Alexander Skjolden <alex@plastboks.net>
2014-11-28 14:33:48 +0100

    allow negative numbers as range y
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- src/lispy.c
+++ src/lispy.c
@@ -1455,7 +1455,7 @@ int main(int argc, char** argv)
     /* Define them with the following Language */
     mpca_lang(MPCA_LANG_DEFAULT,
       "                                                           \
-        range    : /-?[0-9]+\\.\\.[0-9]+/;                        \
+        range    : /-?[0-9]+\\.\\.-?[0-9]+/;                      \
         decimal  : /-?[0-9]+\\.[0-9]+/;                           \
         number   : /-?[0-9]+/ ;                                   \
         string   : /\"(\\\\.|[^\"])*\"/ ;                         \
