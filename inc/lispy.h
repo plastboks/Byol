656a209bff9be70f4ea018a35b900a152805205d
Alexander Skjolden <alex@plastboks.net>
2014-11-28 14:51:41 +0100

    moved macros to seperate file
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- inc/lispy.h
+++ inc/lispy.h
@@ -38,28 +38,7 @@
 #include <math.h>
 
 #include "structures.h"
-
-#define LASSERT(args, cond, fmt, ...) \
-    if (!(cond)) { \
-        lval* err = lval_err(fmt, ##__VA_ARGS__); \
-        lval_del(args); \
-        return err; \
-    }
-
-#define LASSERT_TYPE(builtin, args, index, expect) \
-    LASSERT(args, args->cell[index]->type == expect, \
-            "Function '%s' passed incorrect type for argument %i. Got %s, expected %s.",\
-            builtin, index, ltype_name(args->cell[index]->type), ltype_name(expect))
-
-#define LASSERT_NUM(builtin, args, num) \
-    LASSERT(args, args->count == num, \
-            "Function '%s' passed incorrect number for arguments. Got %i, expected %i.",\
-            builtin, args->count, num)
-
-#define LASSERT_NOT_EMPTY(builtin, args, index) \
-    LASSERT(args, args->cell[index]->count != 0, \
-            "Function '%s' passed {} for argument %i.",\
-            builtin, index)
+#include "macros.h"
 
 lval* lval_eval(lenv* e, lval* v);
 lval* lval_eval_sexpr(lenv*e, lval* v);
