06c3d6cded495b29dd3f97d66c960fd07a8439db
Alexander Skjolden <alex@plastboks.net>
2014-11-28 20:21:10 +0100

    builtins source and header file
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- inc/lval.h
+++ inc/lval.h
@@ -30,11 +30,7 @@
  *
  */
 
-#include <string.h>
-#include <math.h>
-
 #include "structures.h"
-#include "func.h"
 #include "macros.h"
 #include "lispy.h"
 #include "mpc.h"
@@ -66,52 +62,6 @@ lval* lval_read_dec(mpc_ast_t* t);
 lval* lval_read_range(mpc_ast_t* t);
 lval* lval_read_str(mpc_ast_t* t);
 lval* lval_read(mpc_ast_t* t);
-
-lval* builtin_head(lenv* e, lval* a);
-lval* builtin_tail(lenv* e, lval* a);
-lval* builtin_cons(lenv* e, lval* a);
-lval* builtin_take(lenv* e, lval* a);
-lval* builtin_drop(lenv* e, lval* a);
-lval* builtin_rev(lenv* e, lval* a);
-lval* builtin_sort(lenv* e, lval* a);
-lval* builtin_last(lenv* e, lval* a);
-lval* builtin_list(lenv* e, lval* a);
-lval* builtin_eval(lenv* e, lval* a);
-lval* builtin_join(lenv* e, lval* a);
-lval* builtin_len(lenv* e, lval* a);
-lval* builtin_init(lenv* e, lval* a);
-lval* builtin_sum(lenv* e, lval* a);
-lval* builtin_prod(lenv* e, lval* a);
-lval* builtin_nth(lenv* e, lval* a);
-lval* builtin_lambda(lenv* e, lval* a);
-lval* builtin_op(lenv* e, lval* a, char* op);
-lval* builtin_ord(lenv* e, lval* a, char* op);
-lval* builtin_gt(lenv* e, lval* a);
-lval* builtin_lt(lenv* e, lval* a);
-lval* builtin_ge(lenv* e, lval* a);
-lval* builtin_le(lenv* e, lval* a);
-lval* builtin_cmp(lenv* e, lval* a, char* op);
-lval* builtin_eq(lenv* e, lval* a);
-lval* builtin_ne(lenv* e, lval* a);
-lval* builtin_and(lenv* e, lval* a);
-lval* builtin_or(lenv* e, lval* a);
-lval* builtin_not(lenv* e, lval* a);
-lval* builtin_if(lenv* e, lval* a);
-lval* builtin_load(lenv* e, lval* a);
-lval* builtin_print(lenv* e, lval* a);
-lval* builtin_exit(lenv* e, lval* a);
-lval* builtin_env(lenv* e, lval* a);
-lval* builtin_error(lenv* e, lval* a);
-lval* builtin_def(lenv* e, lval* a);
-lval* builtin_put(lenv* e, lval* a);
-lval* builtin_add(lenv* e, lval* a);
-lval* builtin_sub(lenv* e, lval* a);
-lval* builtin_mul(lenv* e, lval* a);
-lval* builtin_div(lenv* e, lval* a);
-lval* builtin_mod(lenv* e, lval* a);
-lval* builtin_pow(lenv* e, lval* a);
-lval* builtin_min(lenv* e, lval* a);
-lval* builtin_max(lenv* e, lval* a);
 
 void lval_del(lval* v);
 char* ltype_name(int t);
