f97c537c3c139e50ef8ad9aa1cf51afc73b9f97e
Alexander Skjolden <alex@plastboks.net>
2015-01-19 10:54:43 +0100

    blank version source file
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

---
+++ src/version.c
@@ -0,0 +1,35 @@
+/*
+ * Lispy version source file
+ *
+ * @filename: version.h
+ *
+ * @version: 0.16
+ *
+ * @date: 2014-11-14
+ *
+ * @description: Lispy version file
+ *
+ * @author: Alexander Skjolden
+ *
+ * @webpage: https://github.com/plastboks/Lispy
+ *
+ * This file is part of Lispy.
+ *
+ * Lispy is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Lispy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Lispy.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "version.h"
+const char * build_date = "";
+const char * build_git_sha = "";
